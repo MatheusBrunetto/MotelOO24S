ab1ca0caad2ac4fc885c3265fd8dda9ebb367340
matheusbrunetto <brunetto@bitzsoftwares.com.br>
2022-05-02 21:58:13 -0300

    Inicial

---
+++ src/main/java/com/utfpr/moteloo24s/MotelOo24SApplication.java
@@ -0,0 +1,13 @@
+package com.utfpr.moteloo24s;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotelOo24SApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MotelOo24SApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/utfpr/moteloo24s/MotelOo24SApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utfpr.moteloo24s;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotelOo24SApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
